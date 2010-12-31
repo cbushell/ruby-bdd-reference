65036426d40cf0b5ba8408ee35407a2c223ec746
cbushell <c.bushell@gmail.com>
2010-12-31 14:36:34 +1100

    make feature fail

--- features/step_definitions/developer_steps.rb
+++ features/step_definitions/developer_steps.rb
@@ -5,5 +5,5 @@ When /^I use this project template$/ do
 end
 
 Then /^I should be able to get some work done$/ do
-  Example::Developer.be_productive
+  Example::Developer.be_productive.should be_true
 end
